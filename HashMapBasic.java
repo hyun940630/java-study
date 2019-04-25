5f6ce9d2850f71b0e3f78c39257bc5893d3f1e1e
string <hh940630@gmail.com>
2019-04-25 18:50:34 +0900

    modified HashMapBasic.java commit

--- HashMapBasic.java
+++ HashMapBasic.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
 
 public class HashMapBasic {
 	public static void main(String[] args) {
@@ -14,6 +16,30 @@ public class HashMapBasic {
 		// get Value
 		int sugar = productPrice.get("Sugar");
 		System.out.println("The price of Sugar is " + sugar + ".");
+		
+		// print all the keys
+		Set <String> keys = productPrice.keySet();
+		
+		for (String key : keys)
+			System.out.println(key);
+		// or (Lambda expression)
+		keys.forEach(key -> System.out.println(key));
+		
+		// print all the Values
+		Collection <Integer> values = productPrice.values();
+		values.forEach(value -> System.out.println(values));
+		
+		// print all the keys and value
+		Set <Map.Entry<String, Integer>> entries = productPrice.entrySet();
+		
+		for (Map.Entry<String, Integer> entry : entries) {
+			System.out.print("Key : " + entry.getKey());
+			System.out.println("Value : " + entry.getValue());
+		}
+		// or (Lambda Expression)
+		productPrice.forEach((key, value) -> {
+			System.out.print("Key : " + key);
+			System.out.println("Value : " + value);
+		});
 	}
-
 }
